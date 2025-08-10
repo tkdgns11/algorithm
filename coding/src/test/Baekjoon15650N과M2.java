1f09ca819897bd202912cac279f1f8686dd4b90b
tkdgns11 <tkdgns1998@gmail.com>
2025-08-10 11:17:05 +0900

    백준 N과M 시리즈

---
+++ coding/src/test/Baekjoon15650N과M2.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+   1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+   고른 수열은 오름차순이어야 한다.
+ */
+class Maekjoon15650N과M2 {
+	static boolean[] visited;
+	static int N;
+	static int M;
+	static int[] numbers;
+	static BufferedWriter bw;
+
+	static void print() throws IOException {
+		for (int i = 0; i < M; i++) {
+			bw.write("" + numbers[i] + " ");
+		}
+		bw.write("\n");
+	}
+
+	static void dfs(int depth, int start) throws IOException {
+		if (depth == M) {
+			print();
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			if (!visited[i]) { // 현재 경로에서 이미 쓴 숫자는 다시 못 쓰게 막는다. (중복 방지)
+				visited[i] = true;
+				numbers[depth] = i;
+				dfs(depth + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[N + 1];
+		numbers = new int[M];
+
+		// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+		dfs(0, 1);
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
---
+++ coding/src/test/Baekjoon15652N과M4.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+   1부터 N까지 자연수 중에서 M개를 고른 수열
+   같은 수를 여러 번 골라도 된다.
+   고른 수열은 비내림차순이어야 한다.
+   길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+ */
+class Baekjoon15652N과M4 {
+	static int N;
+	static int M;
+	static int[] numbers;
+	static BufferedWriter bw;
+
+	static void print() throws IOException {
+		for (int i = 0; i < M; i++) {
+			bw.write("" + numbers[i] + " ");
+		}
+		bw.write("\n");
+	}
+
+	static void dfs(int depth, int start) throws IOException {
+		if (depth == M) {
+			print();
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			numbers[depth] = i;
+			dfs(depth + 1, i);
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		numbers = new int[M];
+
+		// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+		dfs(0, 1);
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
