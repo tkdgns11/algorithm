aa6d0a64c96e0cedff1921e6cb68b5dab7e64952
SSAFY <SSAFY@DESKTOP-763H707>
2025-08-14 17:54:20 +0900

    20250814 17:54

---
+++ coding/src/test/SW모의_벌꿀채취.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.*;
+import java.io.*;
+
+public class SW모의_벌꿀채취 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for(int tc = 1; tc<=T; tc++) {
+			
+		}
+	}
+}
