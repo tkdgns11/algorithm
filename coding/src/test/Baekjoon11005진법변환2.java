5f3e066cca81c48dafd89495c93e564323194ee6
tkdgns11 <tkdgns1998@gmail.com>
2025-08-22 18:20:52 +0900

    tmp

--- coding/src/test/Baekjoon11005진법변환2.java
+++ coding/src/test/Baekjoon11005진법변환2.java
@@ -3,15 +3,25 @@ package test;
 import java.io.*;
 import java.util.*;
 
-public class Asseting {
+public class Baekjoon11005진법변환2 {
 
 	public static void main(String[] args) throws IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		int B = Integer.parseInt(st.nextToken());
+		
+		char[] charArr = new char[36];
+		for(int i=0; i<charArr.length;i++) {
+			if(i >=0 && i<=9) {
+				charArr = 
+			}
+		}
+		
 	}
-
 }
