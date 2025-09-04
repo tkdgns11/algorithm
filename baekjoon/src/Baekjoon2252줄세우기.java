a1c6531d459ba8692772ac6e30e307ee64c2b3ff
윤상훈 <tkdgns1998@naver.com>
2025-09-04 18:00:27 +0900

    0904

--- baekjoon/src/Baekjoon2252줄세우기.java
+++ baekjoon/src/Baekjoon2252줄세우기.java
@@ -4,29 +4,54 @@ import java.io.*;
 import java.util.*;
 
 public class Baekjoon2252줄세우기 {
+	
+	static int N;
+	static List<Integer>[] pan;
+	static boolean[] visited;
+	static BufferedWriter bw;
 
+	static void dfs(int node) throws IOException {
+		for(int i=0; i<pan[node].size(); i++) {
+			if(!visited[pan[node].get(i)]) {
+				visited[pan[node].get(i)] = true;
+				dfs(pan[node].get(i));
+			}
+		}
+		bw.write(node + " ");
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] pan = new int[N+1][N+1];
+		// Arrays.fill(pan, new ArrayList<>());
+		pan = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) pan[i] = new ArrayList<>();
 		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			// num1이 num2보다 앞에 있어야 한다.
 			int num1 = Integer.parseInt(st.nextToken());
 			int num2 = Integer.parseInt(st.nextToken());
-			
-			pan[num1][num2] = 1;
+			pan[num2].add(num1);
 		}
 		
+		visited = new boolean[N+1];
 		
-
+		for(int i=1; i<=N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				dfs(i);
+			}
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
---
+++ baekjoon/src/Baekjoon2623음악프로그램.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon2623음악프로그램 {
+
+	public static void main(String[] args) {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+	}
+
+}
