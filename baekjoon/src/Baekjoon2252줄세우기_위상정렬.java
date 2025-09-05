30d5f207090824b15b66fff8e1bf4543c2207978
tkdgns11 <tkdgns1998@gmail.com>
2025-09-05 19:07:57 +0900

    ㅁㄴㅇㄹ

--- baekjoon/src/Baekjoon2252줄세우기_위상정렬.java
+++ baekjoon/src/Baekjoon2252줄세우기_위상정렬.java
@@ -3,65 +3,52 @@ package src;
 import java.io.*;
 import java.util.*;
 
-public class Baekjoon2623음악프로그램 {
-
-	public static void main(String[] args) throws IOException {
-
+public class Baekjoon2252줄세우기_위상정렬 {
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		List<Integer>[] pan = new ArrayList[N+1];
-		
-		for(int i=1; i<=N; i++) {
-			pan[i] = new ArrayList<>();
-		}
-		
+		List<Integer>[] pan = new ArrayList[N + 1];
 		int[] indegree = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) pan[i] = new ArrayList<>();
 		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			
-			int singers = Integer.parseInt(st.nextToken());
-			
-			int start = Integer.parseInt(st.nextToken());
-			
-			for(int j=2; j<=singers; j++) {
-				int end = Integer.parseInt(st.nextToken());
-				pan[start].add(end);
-				indegree[end]++;
-				start = end;
-			}
+			// num1이 num2보다 앞에 있어야 한다.
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			pan[num1].add(num2);
+			indegree[num2]++;
 		}
 		
 		Queue<Integer> q = new LinkedList<>();
 		
-		for(int i=1; i<= N; i++) {
+		for(int i=1; i<=N; i++) {
 			if(indegree[i] == 0) {
 				q.add(i);
 			}
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		int count = 0;
 		while(!q.isEmpty()) {
 			int node = q.poll();
-			sb.append(node + "\n");
-			count++;
+			bw.write(Integer.toString(node));
+			bw.write(' ');
 			
 			for(int i=0; i<pan[node].size(); i++) {
 				int next = pan[node].get(i);
-				indegree[next]--;
+					indegree[next]--;
 				if(indegree[next] == 0) {
 					q.offer(next);
 				}
 			}
 		}
-		bw.write(count == N ? sb.toString() : "0");
+		
 		bw.flush();
 		bw.close();
 		br.close();
