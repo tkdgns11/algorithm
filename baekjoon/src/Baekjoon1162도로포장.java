a1e60bb6179fd700dc421d6b05d37512975d4a38
tkdgns11 <tkdgns1998@gmail.com>
2025-09-09 00:07:16 +0900

    백준 1162 도로포장 풀이중

---
+++ baekjoon/src/Baekjoon1162도로포장.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon1162도로포장 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+
+	}
+
+}
