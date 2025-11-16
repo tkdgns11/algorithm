764816d2f7e61d6754ab662cee013b555add9fa7
tkdgns11 <tkdgns1998@gmail.com>
2025-11-16 21:51:12 +0900

    1116

--- baekjoon/src/Baekjoon11725트리의부모찾기bfs.java
+++ baekjoon/src/Baekjoon11725트리의부모찾기bfs.java
@@ -3,29 +3,29 @@ package src;
 import java.io.*;
 import java.util.*;
 
-public class Beakjoon11725트리의부모찾기bfs {
+public class Baekjoon11725트리의부모찾기bfs {
 	
-	static List[] Nodes;
+	static List<Integer>[] nodes;
 	static int[] parent;
-	static boolean[] visited;
 	
 	static void bfs(int root) {
 		Queue<Integer> q = new LinkedList<>();
+		
+		parent[root] = 1;
 		
 		q.add(root);
 		
 		while(!q.isEmpty()) {
 			int current = q.poll();
 			
-			for(int i=0; i<Nodes[current].size(); i++) {
-				if() {
-					
+			for(int i=0; i<nodes[current].size(); i++) {
+				int child = nodes[current].get(i);
+				if(parent[child] == 0) {
+					parent[child] = current;
+					q.add(child);
 				}
 			}
-			
 		}
-		
-	
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -35,30 +35,31 @@ public class Beakjoon11725트리의부모찾기bfs {
 		
 		int N = Integer.parseInt(br.readLine().trim());
 		
-		Nodes = new ArrayList[N+1];
+		nodes = new ArrayList[N+1];
 		parent = new int[N+1];
-		visited = new boolean[N+1];
 		
 		for(int i=1; i<=N; i++) {
-			Nodes[i] = new ArrayList<>();
+			nodes[i] = new ArrayList<>();
 		}
 		
-		for(int i=0; i<N-1; i++) {
+		for(int i=1; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			int one = Integer.parseInt(st.nextToken());
 			int two = Integer.parseInt(st.nextToken());
 			
-			Nodes[one].add(two);
-			Nodes[two].add(one);
+			nodes[one].add(two);
+			nodes[two].add(one);
 		}
 		
 		bfs(1);
 		
-		
+		for(int i=2; i<parent.length; i++) {
+			bw.write(Integer.toString(parent[i]));
+			bw.write('\n');
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-	
-	
-	
-
 }
